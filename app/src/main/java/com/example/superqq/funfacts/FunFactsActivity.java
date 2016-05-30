d86e50bad60c5f72dd2bb1a5d7d4149cc4fd8211
SUPERQQ <442652348@qq.com>
2016-05-31 00:45:45 +0800

    button finished

--- app/src/main/java/com/example/superqq/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/superqq/funfacts/FunFactsActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class FunFactsActivity extends AppCompatActivity {
     //Declare our view variable
     private TextView mFactTextView;
@@ -22,9 +24,23 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fact = "Ostriches can run faster than horses";
-                mShowFactButton.setText(fact);
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
 
+                String fact = "";
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(facts.length);
+                fact = facts[randomNumber];
+                mFactTextView.setText(fact);
             }
         };
         mShowFactButton.setOnClickListener(listener);
